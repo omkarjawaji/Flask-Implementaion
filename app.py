49386b4b6415bbfc576333bc25ef2dfbef5ca283
Omkar Jawaji <omkarjawaji9@gmail.com>
2021-09-17 11:45:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+    #return "<p>Hello, World!</p>"
+
+@app.route("/products")
+def products():
+    return "<p>It's a products page!</p>"
+
+if __name__ == "__main__":
+    app.run(debug = True, port = 8000)
+
